e3066570b739db0e1bc674c2c363e1c1be027fcd
MyshaAhmed <mysha7ahmed@gmail.com>
2025-03-20 15:13:06 +0600

    learned style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+//Bacic Concepts:
+// ======================================================>
+
   // variable:
   var name='Mysha Ahmed';
   var age=25;
@@ -11,16 +14,26 @@ function App() {
     name: 'Mysha Ahmed',
     age: 25,
   }
+
   // array:
   var numbers=[1,2,3,4,5];
+
   // function:
   var greet=function(){
     console.log('Hello World!');
   }
+
   // conditional rendering:
   var isAdult=age>=18;
   // ternary operator:
   var greeting=isAdult?'Welcome':'Sorry, you are too young.';
+
+// Style object:
+//===========================================================
+  var style={
+    color: 'red',
+    backgroundColor: 'pink',
+  }
 
   // JSX:
   return (
@@ -32,8 +45,8 @@ function App() {
           <br />  How old am I? <br />  {age} Years old.
         </p>
         <h3>A question For you Now ✨</h3>
-        <p>Do you know {person.name}? </p>
-        <p>Yes, she is {person.age} years old. </p>
+        <p style={style}>Do you know {person.name}? </p>
+        <p style={{backgroundColor: 'cyan',color: 'blue'}}>Yes, she is {person.age} years old. </p> /* inline style*/
         <p>The sum of the numbers in the array is {numbers.reduce((a,b)=>a+b,0)}. </p>
         <button onClick={greet}>Greet</button>
         <p>{greeting}</p>
