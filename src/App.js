b8d090d2cfb9e2153e97aa925b5746fdd3882e63
MyshaAhmed <mysha7ahmed@gmail.com>
2025-03-20 15:00:09 +0600

    basic concepts done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // variable:
   var name='Mysha Ahmed';
   var age=25;
+
+  // object:
+  var person={
+    name: 'Mysha Ahmed',
+    age: 25,
+  }
+  // array:
+  var numbers=[1,2,3,4,5];
+  // function:
+  var greet=function(){
+    console.log('Hello World!');
+  }
+  // conditional rendering:
+  var isAdult=age>=18;
+  // ternary operator:
+  var greeting=isAdult?'Welcome':'Sorry, you are too young.';
+
+  // JSX:
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +31,12 @@ function App() {
           Hi, I am {name}! 🤗
           <br />  How old am I? <br />  {age} Years old.
         </p>
-        
+        <h3>A question For you Now ✨</h3>
+        <p>Do you know {person.name}? </p>
+        <p>Yes, she is {person.age} years old. </p>
+        <p>The sum of the numbers in the array is {numbers.reduce((a,b)=>a+b,0)}. </p>
+        <button onClick={greet}>Greet</button>
+        <p>{greeting}</p>
       </header>
     </div>
   );
