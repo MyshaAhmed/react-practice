167f92957601f6e1f11cd610bdebe825fa610de8
MyshaAhmed <mysha7ahmed@gmail.com>
2025-03-20 14:52:08 +0600

    more variables

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   var name='Mysha Ahmed';
+  var age=25;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi, I am {name}! 🤗
+          <br />  How old am I? <br />  {age} Years old.
         </p>
         
       </header>
